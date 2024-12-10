83ec7300d8a091f241ac4daca90784e102b8813e
Jirat Boomuang <jiratb@outlook.com>
2024-12-11 05:26:44 +0800

    Commit config.py

---
+++ DBA861/PneumoniaDetection/config.py
@@ -0,0 +1,10 @@
+MODELS_ROOT = f"./PneumoniaDetection/models/"
+DATA_ROOT = ""
+
+RESNET50 = f"pneumonia_detector_resnet50.h5"
+VGG19 = f"pneumonia_detector_vgg19.h5"
+CNN = f"pneumonia_detector_cnn.h5"
+
+TRAINING_FOLDER = ""
+VALIDATION_FOLDER = ""
+TEST_FOLDER = ""
